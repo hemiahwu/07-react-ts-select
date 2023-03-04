110351155dafac9da1a56b698c9b6be83509c4f9
吴海洋 <why@vip.local>
2023-03-04 15:55:41 +0800

    05

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,4 +1,5 @@
 // import "./select.module.css";
+import { useState } from "react";
 import styles from "./select.module.css";
 import { SelectOption } from "./types";
 
@@ -9,17 +10,25 @@ type SelectProps = {
 };
 
 export function Select({ value, onChange, options }: SelectProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   const selectOption = (option: SelectOption) => {
     onChange(option);
   };
   return (
     <>
-      <div className={styles.container}>
+      <div
+        className={styles.container}
+        onClick={() => setIsOpen((prev) => !prev)}
+        onBlur={() => setIsOpen(false)}
+        tabIndex={0}
+      >
         <span className={styles.value}>{value.label}</span>
         <button className={styles["clear-btn"]}>&times;</button>
         <div className={styles.divider}></div>
         <div className={styles.caret}></div>
-        <ul className={`${styles.options} ${styles.show}`}>
+
+        <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
           {options.map((option: SelectOption, i: number) => (
             <li
               onClick={(e) => {
